94fc4598f693d8dcc9d53622cf55c815ec454289
Renyuan Lyu <renyuan.lyu@gmail.com>
2023-04-02 16:48:51 +0800

    update Back-prop

--- _code_/Medium-Python-Neural-Network-master/example_xor.py
+++ _code_/Medium-Python-Neural-Network-master/example_xor.py
@@ -7,20 +7,39 @@ from activations import tanh, tanh_prime
 from losses import mse, mse_prime
 
 # training data
-x_train = np.array([[[0,0]], [[0,1]], [[1,0]], [[1,1]]])
-y_train = np.array([[[0]], [[1]], [[1]], [[0]]])
+x_train= np.array([
+    [[0,0]], 
+    [[0,1]], 
+    [[1,0]], 
+    [[1,1]]])
+
+y_train= np.array([
+    [[0]], 
+    [[1]], 
+    [[1]], 
+    [[0]]])
 
 # network
-net = Network()
-net.add(FCLayer(2, 3))
-net.add(ActivationLayer(tanh, tanh_prime))
-net.add(FCLayer(3, 1))
-net.add(ActivationLayer(tanh, tanh_prime))
+net= Network()
+
+net.add(FCLayer(2, 3)); net.add(ActivationLayer(tanh, tanh_prime))
+net.add(FCLayer(3, 1)); net.add(ActivationLayer(tanh, tanh_prime))
+
+# test , before training
+out = net.predict(x_train)
+print(out)
+
 
 # train
 net.use(mse, mse_prime)
-net.fit(x_train, y_train, epochs=1000, learning_rate=0.1)
 
-# test
+net.fit(
+    x_train, 
+    y_train, 
+    epochs= 1000, 
+    learning_rate= 0.1
+    )
+
+# test, after training
 out = net.predict(x_train)
 print(out)
--- _code_/Medium-Python-Neural-Network-master/losses.py
+++ _code_/Medium-Python-Neural-Network-master/losses.py
@@ -1,8 +1,15 @@
 import numpy as np
 
 # loss function and its derivative
-def mse(y_true, y_pred):
+def mse(y_true, y_pred): # mean squared error
     return np.mean(np.power(y_true-y_pred, 2))
 
-def mse_prime(y_true, y_pred):
+def mse_prime(y_true, y_pred): # mean squared error derivative
     return 2*(y_pred-y_true)/y_true.size
+
+def sse(y_true, y_pred): # sum of squared errors
+    e= np.sum((y_pred- y_true)**2)
+    return e
+
+def sse_prime(y_true, y_pred): #
+    return 2*(y_pred-y_true)
